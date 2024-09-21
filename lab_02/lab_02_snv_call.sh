62725c7fefa1191f438f1f478d085bee607e9ae0
Jinhyun Kim <jinhyun9130@gmail.com>
2024-09-21 20:53:31 +0900

    lab_02 prepared

---
+++ lab_02/lab_02_snv_call.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+## Example usage:
+## ./lab_02_snv_call.sh hl60_brca1 ./resources ./intermediates ./results
+
+## This script performs single nucleotide variant (SNV) calling on input paired-end sequencing data (fastq.gz files)
+## The pipeline includes quality control, adapter trimming, read alignment, duplicate marking, and variant calling.
+## Key tools used: FastQC, Cutadapt, BWA, Samtools, GATK, Picard.
+
+
+# Positional arguments and variables
+SAMPLE_NAME=$1         # Name of the sample. Example: hl60_brca1
+INPUT_DIR=$2           # Directory that store input fastq.gz files and reference genome
+INTERMEDIATE_DIR=$3    # Directory for storing intermediate files
+RESULTS_DIR=$4         # Directory for storing final results
+
+# Define input files
+READ1=$INPUT_DIR/"$SAMPLE_NAME"_R1.fastq.gz
+READ2=$INPUT_DIR/"$SAMPLE_NAME"_R2.fastq.gz
+REFERENCE_GENOME=$INPUT_DIR/reference_genome/chr17_subset.fa
+
+# Create necessary directories
+mkdir -p $INTERMEDIATE_DIR
+mkdir -p $RESULTS_DIR/FastQC
+
+# Define summary file
+SUMMARY_FILE=$RESULTS_DIR/QC_summary.txt
+
+
+
+##### 실습1. $READ1 파일 첫번쨰 NGS read의 read length를 계산하여 QC_summary $SUMMARY_FILE 파일에 저장하라
+
+
+##### 실습2. FastQC를 수행하고 결과를 디렉토리 $RESULTS_DIR/FastQC에 저장하라
+
+
+
+##### 실습3-1. Cutadapt를 이용해 $READ1파일과 $READ2파일에 대하여 Illumina adapter 및 low quality base trimming을 수행하여 결과를 $INTERMEDIATE_DIR폴더에 $SAMPLE_NAME.trimmed_R1.fastq.gz, $SAMPLE_NAME.trimmed_R2.fastq.gz의 파일이름으로 저장하라
+
+    
+##### 실습3-2. 두개의 trimmed 파일에 대해 FastQC를 수행하여 결과를 $RESULTS_DIR/FastQC에 저장하라
+READ1_TRIMMED=$INTERMEDIATE_DIR/$SAMPLE_NAME.trimmed_R1.fastq.gz
+READ2_TRIMMED=$INTERMEDIATE_DIR/$SAMPLE_NAME.trimmed_R2.fastq.gz
+
+
+##### 실습3-3. $READ1_TRIMMED 파일에 대한 평균 read length를 계산하여 $SUMMARY_FILE 파일에 "append"하라
+
+
+
+##### 실습 4. bwa index, samtools faidx, gatk CreateSequenceDictionary 3가지 툴로 reference genome을 인덱싱하라
+
+
+
+##### 실습 5-1. bwa mem 알고리즘을 이용하여 $READ1_TRIMMED $READ2_TRIMMED 파일을 reference genome에 align하고 결과를 $INTERMEDIATE_DIR/$SAMPLE_NAME.aligned.sam 파일에 저장하라
+RG="@RG\tID:$SAMPLE_NAME\tSM:$SAMPLE_NAME\tLB:LIB_$SAMPLE_NAME\tPL:ILLUMINA"
+
+
+##### 실습 5-2. $INTERMEDIATE_DIR/$SAMPLE_NAME.aligned.sam파일에서 전체 read수를 출력하여 $SUMMARY_FILE에 append하라
+
+
+##### 실습 5-3. Mapping quality가 30 이상인 read 수를 계산하여 $SUMMARY_FILE에 append하라
+
+
+
+##### 실습 6. samtools sort명령어를 사용하여 aligned.sam파일을 coordinate sorting하고 결과를 BAM형식으로 출력하여 $INTERMEDIATE_DIR/$SAMPLE_NAME.sorted.bam 파일에 저장하라
+
+
+
+##### 실습 7-1. Picard MarkDuplicates를 이용하여 sorted.bam파일에 PCR duplicate을 마킹하고 결과파일은 $INTERMEDIATE_DIR/$SAMPLE_NAME.markdup.bam 파일에, metric file은 $INTERMEDIATE_DIR/$SAMPLE_NAME.markdup_metrics.txt에 저장하라
+ 
+
+##### 실습 7-2. $PREPROCESSED_BAM_FILE 파일로 부터 PCR duplicate rate를 계산하여 $SUMMARY_FILE에 값을 append하라
+PREPROCESSED_BAM_FILE=$INTERMEDIATE_DIR/$SAMPLE_NAME.markdup.bam
+
+
+
+##### 실습 8-1. HaplotypeCaller를 이용해 $PREPROCESSED_BAM_FILE 파일에 대해 variant call을 수행하고 결과를 $RESULTS_DIR/$SAMPLE_NAME.raw_variants.vcf 파일에 저장하라. reference genome에는 $REFERENCE_GENOME 값을 이용할 것
+
+
+##### 실습 8-2. $RAW_VCF_FILE 파일에서 variant quality > 50 인 record와 header를 출력하여 $FILTERED_VCF 파일에 저장하라
+RAW_VCF_FILE=$RESULTS_DIR/$SAMPLE_NAME.raw_variants.vcf
+FILTERED_VCF=$RESULTS_DIR/$SAMPLE_NAME.filtered_variants.vcf
+
---
+++ lab_02/tests/test_lab_02.py
@@ -0,0 +1,117 @@
+import os, shutil
+from pathlib import Path
+import subprocess
+import pytest
+
+RESOURCE_DIR = Path("./resources")
+
+SAMPLE_NAME = "test_sample1"
+TEST_DIR = Path("./temporary_testdir")
+INPUT_DIR = Path(TEST_DIR) / "inputs"
+INTERMEDIATE_DIR = Path(TEST_DIR) / "intermediates"
+RESULTS_DIR = Path(TEST_DIR) / "results"
+SCRIPT_PATH = "./lab_02_snv_call.sh"
+
+
+@pytest.fixture(scope="module")
+def setup_directories():
+    if os.path.exists(TEST_DIR):
+        print(f"----- Cleaning up previous test_dir before testing : {TEST_DIR}")
+        subprocess.run(["rm", "-rf", TEST_DIR])
+
+    os.makedirs(INPUT_DIR / "reference_genome", exist_ok=True)
+    os.makedirs(INTERMEDIATE_DIR, exist_ok=True)
+    os.makedirs(RESULTS_DIR, exist_ok=True)
+
+    read1 = RESOURCE_DIR / "hl60_brca1_R1.fastq.gz"
+    read2 = RESOURCE_DIR / "hl60_brca1_R2.fastq.gz"
+    reference_genome = RESOURCE_DIR / "reference_genome" / "chr17_subset.fa"
+    shutil.copy(read1, INPUT_DIR / f"{SAMPLE_NAME}_R1.fastq.gz")
+    shutil.copy(read2, INPUT_DIR / f"{SAMPLE_NAME}_R2.fastq.gz")
+    shutil.copy(reference_genome, INPUT_DIR / "reference_genome")
+    print("----- Test file preparation complete")
+
+    yield  # Run the tests
+
+    print("----- All test run finished. Cleaning up test_dir after testing : {TEST_DIR}")
+    subprocess.run(["rm", "-rf", TEST_DIR])
+
+def test_script_execution_score_5(setup_directories):
+    result = subprocess.run([SCRIPT_PATH, SAMPLE_NAME, INPUT_DIR, INTERMEDIATE_DIR, RESULTS_DIR], 
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    assert result.returncode == 0, f"Script failed to run: {result.stderr.decode()}"
+
+def test_summary_file_score_5():
+    # Check if the QC_summary.txt file is created
+    summary_file = RESULTS_DIR / "QC_summary.txt"
+    assert summary_file.exists(), "QC_summary.txt was not created on expected location"
+
+    expected_values = [151, 147.538, 5242, 5238, 0.0354826]
+    with open(summary_file, "r") as f:
+        contents = f.read().splitlines()
+
+    assert len(contents) == len(expected_values), f"Expected {len(expected_values)} lines in 'QC_summary.txt' file, but got {len(contents)}"
+    
+    for i, expected_value in enumerate(expected_values):
+        assert float(contents[i].strip()) == pytest.approx(expected_value, abs=1e-4), f"'QC_summary.txt' file line {i+1} value does not match" #: expected {expected_value}, got {contents[i].strip()}
+
+
+def test_fastqc_results_score_2():
+    # Check if FastQC results were generated
+    fastqc_dir = RESULTS_DIR / "FastQC"
+    assert os.path.exists(fastqc_dir / f"{SAMPLE_NAME}_R1_fastqc.html"), "FastQC R1 report missing"
+    assert os.path.exists(fastqc_dir / f"{SAMPLE_NAME}_R2_fastqc.html"), "FastQC R2 report missing"
+    assert os.path.exists(fastqc_dir / f"{SAMPLE_NAME}.trimmed_R1_fastqc.html"), "FastQC trimmed R1 report missing"
+    assert os.path.exists(fastqc_dir / f"{SAMPLE_NAME}.trimmed_R2_fastqc.html"), "FastQC trimmed R2 report missing"
+
+def test_cutadapt_score_1():
+    trimmed_r1 = INTERMEDIATE_DIR / f"{SAMPLE_NAME}.trimmed_R1.fastq.gz"
+    trimmed_r2 = INTERMEDIATE_DIR / f"{SAMPLE_NAME}.trimmed_R2.fastq.gz"
+    
+    assert trimmed_r1.exists(), "Trimmed R1 fastq file not created"
+    assert trimmed_r2.exists(), "Trimmed R2 fastq file not created"
+
+def test_ref_index_score_2():
+    ref_index1 = INPUT_DIR / "reference_genome/chr17_subset.fa.fai"
+    ref_index2 = INPUT_DIR / "reference_genome/chr17_subset.dict"
+    ref_index3 = INPUT_DIR / "reference_genome/chr17_subset.fa.bwt"
+    assert ref_index1.exists(), "samtools faidx indexing not performed"
+    assert ref_index2.exists(), "gatk CreateSequenceDictionary indexing not performed"
+    assert ref_index3.exists(), "BWA index not created for reference genome"
+
+def test_align_score_1():
+    # Check if the SAM file from BWA MEM was created
+    aligned_sam = INTERMEDIATE_DIR / f"{SAMPLE_NAME}.aligned.sam"
+    assert aligned_sam.exists(), "Aligned SAM file not created"
+
+def test_sort_bam_score_1():
+    sorted_bam = INTERMEDIATE_DIR / f"{SAMPLE_NAME}.sorted.bam"
+    assert sorted_bam.exists(), "Sorted BAM file not created"
+
+def test_mark_duplicates_score_1():
+    markdup_bam = INTERMEDIATE_DIR / f"{SAMPLE_NAME}.markdup.bam"
+    assert markdup_bam.exists(), "MarkDuplicates BAM file not created"
+
+def test_haplotypecaller_vcf_score_1():
+    vcf_file = RESULTS_DIR / f"{SAMPLE_NAME}.raw_variants.vcf"
+    assert vcf_file.exists(), "VCF file not created by HaplotypeCaller"
+
+    with open(vcf_file, "r") as vcf:
+        variant_count = sum(1 for line in vcf if not line.startswith("#"))
+
+    assert variant_count == 37, f"Expected 37 variants at raw_variants.vcf file, but found {variant_count}"
+
+def test_vcf_filter_score_1():
+    vcf_file = RESULTS_DIR / f"{SAMPLE_NAME}.filtered_variants.vcf"
+    assert vcf_file.exists(), "filtered_variants.vcf file not created"
+
+    with open(vcf_file, "r") as vcf:
+        variant_count = sum(1 for line in vcf if not line.startswith("#"))
+        
+    with open(vcf_file, "r") as vcf:
+        header_count = sum(1 for line in vcf if line.startswith("#"))
+
+
+    assert variant_count == 32, f"Expected 32 variants at filtered_variants.vcf file, but found {variant_count}"
+    assert header_count == 26, f"Expected 26 header lines at filtered_variants.vcf file, but found {header_count} lines"
